092eec49747c7652591a822228c84d034d1c973e
Alexandra Veyt <alexandra.pasyuta@gmail.com>
2016-01-04 09:24:17 +0300

    slideshow js ready

--- app/js/main.js
+++ app/js/main.js
@@ -124,7 +124,41 @@ var ViewStateChange = (function(){
 	}
 }());
 
+var Slideshow = (function(){
+
+	var _changeSlide = function($this){
+		var
+			container = $this.closest('.products__slideshow'),
+			path = $this.find('img').attr('src'),
+			display = container.find('.products__slideshow-img');
+
+		$this.closest('.products__slideshow-item').addClass('active')
+			.siblings().removeClass('active');
+
+		display.fadeOut(function() {
+			$(this).attr('src', path).fadeIn();
+		});
+	}
+
+	return {
+		init: function(){
+			$('.products__slideshow-link').on('click', function(e) {
+				e.preventDefault();
+				/* Act on the event */
+				var
+					$this = $(this);
+
+				_changeSlide($this);
+			});
+		}
+	}
+}());
+
 $(document).ready(function(){
+
+	if ($('.products__slideshow').length) {
+		Slideshow.init();
+	}
 
 	ViewStateChange.init();
 
